f2b2ad9132c1e90603c83dba6a8d1714ca650ba7
Rthiago1 <35467590+Rthiago1@users.noreply.github.com>
2023-06-12 11:54:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/",fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # Normalize raw json data
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # transforms normalized fruityvice_response data into a dataframe
